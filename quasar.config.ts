b77c75ad834b09ea13ad4eb44ab21655a0e3e37b
Julien <julienreichel@users.noreply.github.com>
2025-12-23 13:06:28 +0100

    fix: use correct path for the github pages

--- quasar.config.ts
+++ quasar.config.ts
@@ -51,7 +51,7 @@ export default defineConfig((ctx) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: process.env.GITHUB_PAGES ? '/hofman-core-quadrant/' : '/',
+      publicPath: process.env.GITHUB_PAGES ? '/ofman-core-quadrant/' : '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
